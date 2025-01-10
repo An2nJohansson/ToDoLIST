0775c77e69041cf1b14da3329f2e37f4efecccb1
An2nJohansson <an2n_johansson@outlook.com>
2025-01-10 10:16:02 +0100

    update of ToDoclass

--- ToDoAppWPF/ToDoClass.cs
+++ ToDoAppWPF/ToDoClass.cs
@@ -1,13 +1,37 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace ToDoAppWPF
 {
-    internal class ToDoClass
+    //summary
+    // hanterar logik för att ta bort och lägga till To-Do tasks
+    //summary
+    public class ToDoClass
     {
+        //lista för tasks
+        private List<string> tasks = new List<string>();
 
+        public void AddTask(string task)
+        {
+            tasks.Add(task);
+        }
+
+        public void RemoveTask(int index) 
+        {
+            if (index >= 0 && index < tasks.Count)
+            {
+               tasks.RemoveAt(index);
+            }
+            
+        }
+
+        public List<string> GetAllTasks() 
+        {
+            return tasks;
+        }
     }
 }
