c4abba1622ad27ebf95aa9221a6dba8224b502be
An2nJohansson <an2n_johansson@outlook.com>
2025-01-11 15:47:29 +0100

    new test + comments

--- TestingToDoList.cs/ToDoListTests.cs
+++ TestingToDoList.cs/ToDoListTests.cs
@@ -5,9 +5,9 @@ namespace TestingToDoList
 {
     public class ToDoListTests
     {
-        
+
         private ToDoClass _todoList;
-        public ToDoListTests() 
+        public ToDoListTests()
         {
             _todoList = new ToDoClass();
         }
@@ -22,10 +22,10 @@ namespace TestingToDoList
             Assert.Contains(task, tasks);
         }
 
-        
+
         //test för att ta bort en task
         [Fact]
-        public void RemoveTask_ShouldRemoveTaskToList() 
+        public void RemoveTask_ShouldRemoveTaskToList()
         {
             var task = "Task to remove";
             _todoList.AddTask(task);
@@ -34,15 +34,36 @@ namespace TestingToDoList
             Assert.DoesNotContain(task, tasks);
         }
 
+        // test för att ogiltiga "remove" försök inte ska krascha appen
         [Fact]
         public void RemoveTask_InvalidIndex_ShouldNotThrowException()
         {
             var task = "valid task";
             _todoList.AddTask(task);
             _todoList.RemoveTask(8); // invalid index
-            
+
             var tasks = _todoList.GetAllTasks();
             Assert.Single(tasks); // den ursprungliga uppgiften ska fortfarande finnas kvar
         }
+
+        // test för att kolla att listan uppdateras korrekt
+        [Fact]
+        public void CorrectTaskListUpdate()
+        {
+            var task1 = "Uppgift 1";
+            var task2 = "Uppgift 2";
+
+            _todoList.AddTask(task1);
+            _todoList.AddTask(task2);
+
+            var tasks = _todoList.GetAllTasks();
+
+            Assert.Equal(2, tasks.Count);
+            Assert.Contains(task1, tasks);
+            Assert.Contains(task2, tasks);
+        }
+            
+            
+        
     }
 }
